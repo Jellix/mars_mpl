95f6abc1384071ef095b6977e3f60c4054e14fc6
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2018-03-20 15:42:50 +0100

    * Fixed coding standard violations.

--- source/ui/gtk-box-digital_clock.adb
+++ source/ui/gtk-box-digital_clock.adb
@@ -5,120 +5,130 @@ package body Gtk.Box.Digital_Clock is
 
    use type Glib.Gdouble;
 
-   subtype Single_Digit_7_X_5 is LED_Assignment (0 .. 4, 0 .. 6);
-   type Char_Def_String is
-     new String (1 .. Single_Digit_7_X_5'Length (X_Dimension) * Single_Digit_7_X_5'Length (Y_Dimension));
+   Char_Width  : constant := 5;
+   Char_Height : constant := 7;
 
-   function "+" (S : in Char_Def_String) return Single_Digit_7_X_5;
-   function "+" (S : in Char_Def_String) return Single_Digit_7_X_5 is
-      Result : Single_Digit_7_X_5;
+   subtype Char_Width_Index  is Natural range 0 .. Char_Width  - 1;
+   subtype Char_Height_Index is Natural range 0 .. Char_Height - 1;
+
+   subtype Char_7_X_5 is LED_Assignment (Char_Width_Index'Range,
+                                         Char_Height_Index'Range);
+   type Char_Def_String is array (1 .. Char_Width * Char_Height) of Character;
+
+   function "+" (S : in Char_Def_String) return Char_7_X_5;
+   function "+" (S : in Char_Def_String) return Char_7_X_5
+   is
+      Result  : Char_7_X_5;
       X_Width : constant := Result'Length (X_Dimension);
    begin
+      Column_Loop :
       for Y in Result'Range (Y_Dimension) loop
+         Convert_Row :
          declare
-            X_Offset : constant Glib.Gint := Glib.Gint (S'First) + Y * X_Width;
+            X_Offset : constant Positive := S'First + Y * X_Width;
          begin
+            Row_Loop :
             for X in Result'Range (X_Dimension) loop
-               Result (X, Y) := S (Positive (X_Offset + X)) /= ' ';
-            end loop;
-         end;
-      end loop;
+               Result (X, Y) := S (X_Offset + X) /= ' ';
+            end loop Row_Loop;
+         end Convert_Row;
+      end loop Column_Loop;
 
       return Result;
    end "+";
 
-   LED_0 : constant Single_Digit_7_X_5 := +(" XXX " &
-                                            "X   X" &
-                                            "X   X" &
-                                            "X   X" &
-                                            "X   X" &
-                                            "X   X" &
-                                            " XXX ");
+   LED_0 : constant Char_7_X_5 := +(" XXX " &
+                                    "X   X" &
+                                    "X   X" &
+                                    "X   X" &
+                                    "X   X" &
+                                    "X   X" &
+                                    " XXX ");
 
-   LED_1 : constant Single_Digit_7_X_5 := +("  X  " &
-                                            " XX  " &
-                                            "  X  " &
-                                            "  X  " &
-                                            "  X  " &
-                                            "  X  " &
-                                            " XXX ");
+   LED_1 : constant Char_7_X_5 := +("  X  " &
+                                    " XX  " &
+                                    "  X  " &
+                                    "  X  " &
+                                    "  X  " &
+                                    "  X  " &
+                                    " XXX ");
 
-   LED_2 : constant Single_Digit_7_X_5 := +(" XXX " &
-                                            "X   X" &
-                                            "    X" &
-                                            "  XX " &
-                                            " X   " &
-                                            "X    " &
-                                            "XXXXX");
+   LED_2 : constant Char_7_X_5 := +(" XXX " &
+                                    "X   X" &
+                                    "    X" &
+                                    "  XX " &
+                                    " X   " &
+                                    "X    " &
+                                    "XXXXX");
 
-   LED_3 : constant Single_Digit_7_X_5 := +(" XXX " &
-                                            "X   X" &
-                                            "    X" &
-                                            "  XX " &
-                                            "    X" &
-                                            "X   X" &
-                                            " XXX ");
+   LED_3 : constant Char_7_X_5 := +(" XXX " &
+                                    "X   X" &
+                                    "    X" &
+                                    "  XX " &
+                                    "    X" &
+                                    "X   X" &
+                                    " XXX ");
 
-   LED_4 : constant Single_Digit_7_X_5 := +("   X " &
-                                            "  XX " &
-                                            " X X " &
-                                            "X  X " &
-                                            "XXXXX" &
-                                            "   X " &
-                                            "   X ");
+   LED_4 : constant Char_7_X_5 := +("   X " &
+                                    "  XX " &
+                                    " X X " &
+                                    "X  X " &
+                                    "XXXXX" &
+                                    "   X " &
+                                    "   X ");
 
-   LED_5 : constant Single_Digit_7_X_5 := +("XXXXX" &
-                                            "X    " &
-                                            "XXXX " &
-                                            "    X" &
-                                            "    X" &
-                                            "X   X" &
-                                            " XXX ");
+   LED_5 : constant Char_7_X_5 := +("XXXXX" &
+                                    "X    " &
+                                    "XXXX " &
+                                    "    X" &
+                                    "    X" &
+                                    "X   X" &
+                                    " XXX ");
 
-   LED_6 : constant Single_Digit_7_X_5 := +("  XX " &
-                                            " X   " &
-                                            "X    " &
-                                            "XXXX " &
-                                            "X   X" &
-                                            "X   X" &
-                                            " XXX ");
+   LED_6 : constant Char_7_X_5 := +("  XX " &
+                                    " X   " &
+                                    "X    " &
+                                    "XXXX " &
+                                    "X   X" &
+                                    "X   X" &
+                                    " XXX ");
 
-   LED_7 : constant Single_Digit_7_X_5 := +("XXXXX" &
-                                            "    X" &
-                                            "   X " &
-                                            "  X  " &
-                                            " X   " &
-                                            " X   " &
-                                            " X   ");
+   LED_7 : constant Char_7_X_5 := +("XXXXX" &
+                                    "    X" &
+                                    "   X " &
+                                    "  X  " &
+                                    " X   " &
+                                    " X   " &
+                                    " X   ");
 
-   LED_8 : constant Single_Digit_7_X_5 := +(" XXX " &
-                                            "X   X" &
-                                            "X   X" &
-                                            " XXX " &
-                                            "X   X" &
-                                            "X   X" &
-                                            " XXX ");
+   LED_8 : constant Char_7_X_5 := +(" XXX " &
+                                    "X   X" &
+                                    "X   X" &
+                                    " XXX " &
+                                    "X   X" &
+                                    "X   X" &
+                                    " XXX ");
 
-   LED_9 : constant Single_Digit_7_X_5 := +(" XXX " &
-                                            "X   X" &
-                                            "X   X" &
-                                            " XXXX" &
-                                            "    X" &
-                                            "   X " &
-                                            " XX  ");
+   LED_9 : constant Char_7_X_5 := +(" XXX " &
+                                    "X   X" &
+                                    "X   X" &
+                                    " XXXX" &
+                                    "    X" &
+                                    "   X " &
+                                    " XX  ");
 
-   type LED_Lookup is array (Valid_Digits) of Single_Digit_7_X_5;
+   type LED_Lookup is array (Valid_Digits) of Char_7_X_5;
 
-   Digit_Lookup : constant LED_Lookup := (0 => LED_0,
-                                          1 => LED_1,
-                                          2 => LED_2,
-                                          3 => LED_3,
-                                          4 => LED_4,
-                                          5 => LED_5,
-                                          6 => LED_6,
-                                          7 => LED_7,
-                                          8 => LED_8,
-                                          9 => LED_9);
+   Digit_Lookup : constant LED_Lookup := LED_Lookup'(0 => LED_0,
+                                                     1 => LED_1,
+                                                     2 => LED_2,
+                                                     3 => LED_3,
+                                                     4 => LED_4,
+                                                     5 => LED_5,
+                                                     6 => LED_6,
+                                                     7 => LED_7,
+                                                     8 => LED_8,
+                                                     9 => LED_9);
 
    function Gtk_New
      (Label     : in Glib.UTF8_String;
@@ -163,28 +173,30 @@ package body Gtk.Box.Digital_Clock is
       The_Grid.all.Set_Row_Spacing (Spacing => 0);
 
       for X in This.LED_Matrix'Range (X_Dimension) loop
-         The_Grid.all.Insert_Column (Position => X);
+         The_Grid.all.Insert_Column (Position => Glib.Gint (X));
       end loop;
 
       for Y in This.LED_Matrix'Range (Y_Dimension) loop
-         The_Grid.all.Insert_Row (Position => Y);
+         The_Grid.all.Insert_Row (Position => Glib.Gint (Y));
       end loop;
 
+      Column_Loop :
       for X in This.LED_Matrix'Range (X_Dimension) loop
+         Row_Loop :
          for Y in This.LED_Matrix'Range (Y_Dimension) loop
             This.LED_Matrix (X, Y) := New_LED (On_Color  => On_Color,
                                                Off_Color => Off_Color);
             The_Grid.all.Attach (Child => This.LED_Matrix (X, Y),
-                                 Left  => X,
-                                 Top   => Y);
-         end loop;
-      end loop;
+                                 Left  => Glib.Gint (X),
+                                 Top   => Glib.Gint (Y));
+         end loop Row_Loop;
+      end loop Column_Loop;
 
-      This.LED_Matrix (6,  2).all.Set_State (State => True);
-      This.LED_Matrix (6,  4).all.Set_State (State => True);
-      This.LED_Matrix (20, 2).all.Set_State (State => True);
-      This.LED_Matrix (20, 4).all.Set_State (State => True);
-      This.LED_Matrix (34, 6).all.Set_State (State => True);
+      This.LED_Matrix (1 * Char_Width + 1, 2).all.Set_State (State => True);
+      This.LED_Matrix (1 * Char_Width + 1, 4).all.Set_State (State => True);
+      This.LED_Matrix (3 * Char_Width + 5, 2).all.Set_State (State => True);
+      This.LED_Matrix (3 * Char_Width + 5, 4).all.Set_State (State => True);
+      This.LED_Matrix (5 * Char_Width + 9, 6).all.Set_State (State => True);
 
       --  Force digits to all zero, so we have a defined state.
       for Digit in Digit_Index'Range loop
@@ -210,13 +222,14 @@ package body Gtk.Box.Digital_Clock is
                        Time : in     Duration)
    is
       Temp       : constant Glib.Gdouble := Glib.Gdouble (Time);
-      The_Digits : constant array (Digit_Index) of Valid_Digits :=
-                     (Natural (Glib.Gdouble'Floor (Temp / 3600.0)) mod 10,
-                      Natural (Glib.Gdouble'Floor (Temp /  600.0)) mod  6,
-                      Natural (Glib.Gdouble'Floor (Temp /   60.0)) mod 10,
-                      Natural (Glib.Gdouble'Floor (Temp /   10.0)) mod  6,
-                      Natural (Glib.Gdouble'Floor (Temp /    1.0)) mod 10,
-                      Natural (Glib.Gdouble'Floor (Temp *   10.0)) mod 10);
+      The_Digits : constant Digits_Set
+        := Digits_Set'
+             (H_1    => Natural (Glib.Gdouble'Floor (Temp / 3600.0)) mod 10,
+              M_10   => Natural (Glib.Gdouble'Floor (Temp /  600.0)) mod  6,
+              M_1    => Natural (Glib.Gdouble'Floor (Temp /   60.0)) mod 10,
+              S_10   => Natural (Glib.Gdouble'Floor (Temp /   10.0)) mod  6,
+              S_1    => Natural (Glib.Gdouble'Floor (Temp /    1.0)) mod 10,
+              S_10th => Natural (Glib.Gdouble'Floor (Temp *   10.0)) mod 10);
    begin
       for Digit in Digit_Index'Range loop
          This.Write_Digit (Digit => Digit,
@@ -229,30 +242,32 @@ package body Gtk.Box.Digital_Clock is
                           Num   : in     Valid_Digits;
                           Force : in     Boolean := False)
    is
-      Old_Layout   : Single_Digit_7_X_5 renames
-                       Digit_Lookup (This.Old_Digits (Digit));
-      Digit_Layout : Single_Digit_7_X_5 renames
-                       Digit_Lookup (Num);
+      Old_Layout   : Char_7_X_5 renames Digit_Lookup (This.Old_Digits (Digit));
+      Digit_Layout : Char_7_X_5 renames Digit_Lookup (Num);
       use type Glib.Gint;
    begin
       --  Speed optimization 1: Only change LED state if the digit to be shown
       --  is different from the previously displayed one.
       if Force or else This.Old_Digits (Digit) /= Num then
+         Column_Loop :
          for X in Digit_Layout'Range (X_Dimension) loop
             Set_Column :
             declare
-               Column : constant Glib.Gint := Digit_Offset (Digit) + X;
+               Column : constant Natural := Digit_Offset (Digit) + X;
             begin
+               Row_Loop :
                for Y in Digit_Layout'Range (Y_Dimension) loop
                   --  Speed optimization 2: Only change state of the LED if it's
                   --  different from the previously shown digit.
-                  if Force or else Old_Layout (X, Y) /= Digit_Layout (X, Y) then
+                  if
+                    Force or else (Old_Layout (X, Y) xor Digit_Layout (X, Y))
+                  then
                      This.LED_Matrix (Column, Y).all.Set_State
                        (State => Digit_Layout (X, Y));
                   end if;
-               end loop;
+               end loop Row_Loop;
             end Set_Column;
-         end loop;
+         end loop Column_Loop;
 
          This.Old_Digits (Digit) := Num;
       end if;
--- source/ui/gtk-box-digital_clock.ads
+++ source/ui/gtk-box-digital_clock.ads
@@ -26,26 +26,30 @@ package Gtk.Box.Digital_Clock is
 
 private
 
-   type    Digit_Index  is range 1 .. 6; -- H:MM:SS.T
+   type    Digit_Index  is (H_1, M_10, M_1, S_10, S_1, S_10th); -- H:MM:SS.T
    subtype Valid_Digits is Natural range 0 .. 9;
 
    type Digits_Set is array (Digit_Index) of Valid_Digits;
 
-   type LED_Assignment is array (Glib.Gint range <>,
-                                 Glib.Gint range <>) of Boolean
+   type LED_Assignment is array (Natural range <>,
+                                 Natural range <>) of Boolean
      with Pack => True;
    X_Dimension : constant := 1;
    Y_Dimension : constant := 2;
 
-   type Dot_Matrix is array (Glib.Gint range <>,
-                             Glib.Gint range <>) of
+   subtype Matrix_Width  is Natural range 0 .. 40;
+   subtype Matrix_Height is Natural range 0 .. 6;
+
+   type Dot_Matrix is array (Natural range <>,
+                             Natural range <>) of
      Gtk.Gauge.LED_Rectangular.Gtk_Gauge_LED_Rectangular;
 
    type Gtk_Box_Digital_Clock_Record is new Gtk_Box_Record with
       record
          Grid       : Gtk.Grid.Gtk_Grid;
          Old_Digits : Digits_Set;
-         LED_Matrix : Dot_Matrix (0 .. 40, 0 .. 6);
+         LED_Matrix : Dot_Matrix (Matrix_Width'Range,
+                                  Matrix_Height'Range);
       end record;
 
    procedure Write_Digit (This  : in out Gtk_Box_Digital_Clock_Record;
@@ -57,8 +61,8 @@ private
                      Off_Color : in Gdk.Color.Gdk_Color) return not null
      Gtk.Gauge.LED_Rectangular.Gtk_Gauge_LED_Rectangular;
 
-   Digit_Offset : constant array (Digit_Index) of Glib.Gint :=
-                    (0, 8, 14, 22, 28, 36);
+   type Offset_List is array (Digit_Index) of Natural;
+
    -- 0         1         2         3         4
    -- 01234567890123456789012345678901234567890
    -- .....   ..... .....   ..... .....   .....
@@ -68,5 +72,11 @@ private
    -- ..... . ..... ..... . ..... .....   .....
    -- .....   ..... .....   ..... .....   .....
    -- .....   ..... .....   ..... ..... . .....
+   Digit_Offset : constant Offset_List := Offset_List'(H_1    => 0,
+                                                       M_10   => 8,
+                                                       M_1    => 14,
+                                                       S_10   => 22,
+                                                       S_1    => 28,
+                                                       S_10th => 36);
 
 end Gtk.Box.Digital_Clock;
