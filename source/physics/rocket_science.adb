088a33b2a26d3b21c10fd48c4a5653a144b84129
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2018-04-12 11:07:00 +0200

    + Added drag equation. Idea is to use it for parachute simulation.

--- source/physics/rocket_science.adb
+++ source/physics/rocket_science.adb
@@ -17,4 +17,29 @@ package body Rocket_Science is
           Ln (X => Float (Initial_Wet_Mass / Current_Wet_Mass));
    end Delta_V;
 
+   --  Drag equations...
+   --  See https://www.grc.nasa.gov/www/k-12/airplane/termv.html
+
+   --  Net (downward) force F = D - W (whereas D = drag, W = weight)
+   --  W = m * g (mass * gravitational acceleration)
+   --  D = Cd * r * V² * A / 2 (whereas Cd = drag coefficient, r = gas density, V = velocity, A = surface area).
+   --
+   --  Combining (Cd * r * A) into a single drag constant C, we arrive at:
+   --  D = C * V² / 2
+   --  F = D - W
+   --  a = F / m
+   function Drag (Current_Wet_Mass : in Shared_Types.Mass;
+                  Gravitation      : in Shared_Types.Acceleration;
+                  Velocity         : in Shared_Types.Velocity;
+                  Drag_Constant    : in Float) return Shared_Types.Acceleration
+   is
+      Upwards_Drag    : Float;
+      Downwards_Force : Float;
+   begin
+      Upwards_Drag    := Drag_Constant * Float (Velocity) * Float (Velocity) / 2.0;
+      Downwards_Force := Upwards_Drag - Float (Current_Wet_Mass * Gravitation);
+
+      return Shared_Types.Acceleration (Downwards_Force / Float (Current_Wet_Mass));
+   end Drag;
+
 end Rocket_Science;
--- source/physics/rocket_science.ads
+++ source/physics/rocket_science.ads
@@ -9,4 +9,20 @@ package Rocket_Science is
       Current_Wet_Mass : in Shared_Types.Mass;
       Exhaust_Velocity : in Shared_Types.Velocity) return Shared_Types.Velocity;
 
+   --  Drag equations...
+   --  See https://www.grc.nasa.gov/www/k-12/airplane/termv.html
+
+   --  Net (downward) force F = D - W (whereas D = drag, W = weight)
+   --  W = m * g (mass * gravitational acceleration)
+   --  D = Cd * r * V² * A / 2 (whereas Cd = drag coefficient, r = gas density, V = velocity, A = surface area).
+   --
+   --  Combining (Cd * r * A) into a single drag constant C, we arrive at:
+   --  D = C * V² / 2
+   --  F = D - W
+   --  a = F / m
+   function Drag (Current_Wet_Mass : in Shared_Types.Mass;
+                  Gravitation      : in Shared_Types.Acceleration;
+                  Velocity         : in Shared_Types.Velocity;
+                  Drag_Constant    : in Float) return Shared_Types.Acceleration;
+
 end Rocket_Science;
