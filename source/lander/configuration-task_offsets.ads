02128e253ba8cfc70d90fa934fe1b3ac1a865a28
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2018-05-03 12:10:10 +0200

    * Task interleave times more evenly distributed.

--- source/lander/configuration-task_offsets.ads
+++ source/lander/configuration-task_offsets.ads
@@ -12,22 +12,22 @@ package Configuration.Task_Offsets is
    Altitude_Task : constant Ada.Real_Time.Time_Span := Microseconds (0);
    --  Altitude task runs at exactly activation time.
 
-   Engine_Task : constant Ada.Real_Time.Time_Span   := Microseconds (750);
-   --  The engine task runs with an offset of 1.5 millisecond.
+   Engine_Task : constant Ada.Real_Time.Time_Span   := Microseconds (1400);
+   --  The engine task runs with an offset of 1.4 milliseconds.
 
-   Fuel_Monitor  : constant Ada.Real_Time.Time_Span := Microseconds (1500);
-   --  The engine task runs with an offset of 1.5 millisecond.
+   Fuel_Monitor  : constant Ada.Real_Time.Time_Span := Microseconds (2800);
+   --  The engine task runs with an offset of 2.8 milliseconds.
 
    type TDM_Offsets is
      array (Shared_Types.Legs_Index) of Ada.Real_Time.Time_Span;
 
    TD_Monitor : constant TDM_Offsets :=
-                  TDM_Offsets'(Shared_Types.LL_000 => Microseconds (3000),
-                               Shared_Types.LL_120 => Microseconds (4500),
-                               Shared_Types.LL_240 => Microseconds (6000));
-   --  The touchdown monitors run with an offset of 3, 4.5, and 6 milliseconds.
+                  TDM_Offsets'(Shared_Types.LL_000 => Microseconds (4200),
+                               Shared_Types.LL_120 => Microseconds (5600),
+                               Shared_Types.LL_240 => Microseconds (7000));
+   --  The touchdown monitors run with an offset of 4.2, 5.6, and 7.0 milliseconds.
 
-   SIM_Task : constant Ada.Real_Time.Time_Span := Microseconds (7500);
-   --  The simulator task runs with an offset of 7.5 milliseconds.
+   SIM_Task : constant Ada.Real_Time.Time_Span := Microseconds (8400);
+   --  The simulator task runs with an offset of 8.4 milliseconds.
 
 end Configuration.Task_Offsets;
