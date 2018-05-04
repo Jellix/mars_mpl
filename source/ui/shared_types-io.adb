2d6bf77be084849ddc0ca83fec74ae40a00c4e28
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2018-05-04 11:54:57 +0200

    * Made large values in Image function with unit a bit human readable.

--- source/ui/shared_types-io.adb
+++ source/ui/shared_types-io.adb
@@ -10,16 +10,18 @@ package body Shared_Types.IO is
       package Fixed_IO is new Ada.Text_IO.Fixed_IO (Num => T);
       pragma Warnings (On, "instance does not use primitive operation ""*""");
 
+      Kilo   : constant Boolean := With_Unit and then Value > 10_000.0;
+      Temp   : constant T := (if Kilo then Value / 1_000.0 else Value);
       Aft    : constant Positive := Positive'Min (T'Aft, 3);
       Result : String (1 .. T'Fore + Aft + 1);
    begin
       Fixed_IO.Put (To   => Result,
-                    Item => Value,
+                    Item => Temp,
                     Aft  => Aft,
                     Exp  => 0);
       return Ada.Strings.Fixed.Trim (Source => Result,
                                      Side   => Ada.Strings.Left)
-        & (if With_Unit then " " & Unit else "");
+        & (if With_Unit then (if Kilo then " k" else " ") & Unit else "");
    end Generic_Image;
 
 end Shared_Types.IO;
